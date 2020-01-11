ea15fbfe6e9aee3468b9172c9f792eff4d5b857f
Juan C <juanceballos@pursuit.org>
2020-01-10 23:03:27 -0500

    added ui, attept of populating cv with mtaCard asset

---
+++ CollectionView-Practice/CollectionView-Practice/CardImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardImageCell.swift
+//  CollectionView-Practice
+//
+//  Created by Juan Ceballos on 1/10/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class CardImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+}
--- CollectionView-Practice/CollectionView-Practice/ViewController.swift
+++ CollectionView-Practice/CollectionView-Practice/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mtaCardCollectionView: UICollectionView!
+    
+    var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    let cellIdentifier = "CardCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mtaCardCollectionView.delegate = self
+        mtaCardCollectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        if let flowLayout = self.mtaCardCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.itemSize = CGSize(width: self.mtaCardCollectionView.bounds.width, height: 120)
+        }
     }
 
+    func setupCollectionView()  {
+        
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource    {
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CardImageCell
+        
+        cell.cardImageView.image = #imageLiteral(resourceName: "metrocard")
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate  {
+    
+}
